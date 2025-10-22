29894afeddbe51afc71b4549995bab79d4f954bb
tomperceau <tom.perceau@etu.unilim.fr>
2025-10-22 14:57:11 +0200

    commit 3

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/SAE-301",
+  base: "/SAE-301/",
 });
